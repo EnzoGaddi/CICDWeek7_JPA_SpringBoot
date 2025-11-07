69d5ddd4e31d03d746b518280a2f09b2a935d962
ENZOGADDI-STUDENT <G00431922@atu.ie>
2025-11-07 09:15:40 +0000

    Initialised project and did exercises 3 and 4 to start

---
+++ src/test/java/atu/ie/cicdlab5/CicdLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdlab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
