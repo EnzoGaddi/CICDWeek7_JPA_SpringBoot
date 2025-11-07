448ba672e394e553a3f4d7188db027e87baed189
ENZOGADDI-STUDENT <G00431922@atu.ie>
2025-11-07 09:24:42 +0000

    Finished exercise 5 and 6 by adding person service and repository code

---
+++ src/main/java/atu/ie/cicdlab5/PersonService.java
@@ -0,0 +1,16 @@
+package atu.ie.cicdlab5;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    private final PersonRepository repo;
+
+    public PersonService(PersonRepository repo) {
+        this.repo = repo;
+    }
+
+    public Person create(Person p) {
+        return repo.save(p);
+    }
+}
